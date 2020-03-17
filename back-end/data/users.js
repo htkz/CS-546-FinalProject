b6aee1acb0112875c095f710275db1be7e2aed25
francisfeng <13903131445@163.com>
2020-03-16 23:01:25 -0400

    Revert

--- back-end/data/users.js
+++ back-end/data/users.js
@@ -1,34 +1,21 @@
 const mongoCollections = require('../config/mongoCollection');
 const users = mongoCollections.users;
-const ObjectId = require('mongodb').ObjectId;
 
 let exportedMethods = {
-    async getAllUsers() {
-        const userCollection = await users();
-
-        const allUsers = await userCollection.find({}).toArray();
-
-        if (!allUsers) {
-            throw 'No users in system!';
-        }
-
-        return allUsers;
+    getAllUsers() {
+        return users().then(userCollection => {
+            return userCollection.find({}).toArray();
+        });
     },
-
-    async getUserById(id) {
-        const userCollection = await users();
-
-        id = await this.checkId(id);
-
-        const user = await userCollection.findOne({ _id: id });
-        if (!user) {
-            throw `No user with that ${id}`;
-        }
-
-        return user;
+    getUserById(id) {
+        return users().then(userCollection => {
+            return userCollection.findOne({ _id: id }).then(user => {
+                if (!user) throw 'User not found';
+                return user;
+            });
+        });
     },
-
-    async addUser(userInfo) {
+    addUser(userInfo) {
         return users().then(userCollection => {
             return userCollection
                 .insertOne(userInfo)
@@ -40,23 +27,16 @@ let exportedMethods = {
                 });
         });
     },
-
-    async removeUser(id) {
-        const userCollection = await users();
-
-        id = await this.checkId(id);
-
-        const deleteInfo = await userCollection.removeOne({ _id: id });
-        if (deleteInfo.deletedCount === 0) {
-            throw `Could not delete user with id of ${id}`;
-        }
-
-        return true;
+    removeUser(id) {
+        return users().then(userCollection => {
+            return userCollection.removeOne({ _id: id }).then(deletionInfo => {
+                if (deletionInfo.deletedCount === 0) {
+                    throw `Could not delete user with id of ${id}`;
+                }
+            });
+        });
     },
-
-    async updateUser(id, userInfo) {
-        id = await this.checkId(id);
-
+    updateUser(id, userInfo) {
         return this.getUserById(id).then(currentUser => {
             return userCollection
                 .updateOne({ _id: id }, updatedUser)
@@ -64,16 +44,6 @@ let exportedMethods = {
                     return this.getUserById(id);
                 });
         });
-    },
-
-    async checkId(id) {
-        if (typeof id == 'string') {
-            return ObjectId(id);
-        } else if (typeof id == 'object') {
-            return id;
-        } else {
-            throw new Error('You must provide valid id to search for.');
-        }
     },
 };
 
