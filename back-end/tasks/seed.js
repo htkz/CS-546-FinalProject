3c30ecf64a1e8da0c8e91e50cec5d3abae858df9
htkz <htkzmo@gmail.com>
2020-04-15 00:47:03 -0400

    Add seed for default database

--- back-end/tasks/seed.js
+++ back-end/tasks/seed.js
@@ -1,9 +1,29 @@
 const dbConnection = require('../config/mongoConnection');
 const data = require('../data/');
-const users = data.users;
+const places = data.places;
 
 const main = async () => {
-    console.log('Seeding!');
+    const db = await dbConnection();
+    const allPlaces = await places.getAllPlaces();
+    if (allPlaces.length !== 0) {
+        console.log('Database already exist, stop seeding!');
+        return;
+    }
+    await places.addPlace(
+        'Stevens',
+        'A famous school in New Jersey',
+        '1 Castle Point Terrace, Hoboken',
+        '07310',
+        20000,
+        ['school', 'quiet'],
+        '2020-03-17',
+        400,
+        'stevens.jpg'
+    );
+
+    console.log('Done seeding database');
+
+    await db.serverConfig.close();
 };
 
-main().catch(console.log);
+main();
