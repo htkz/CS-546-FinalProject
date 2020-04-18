4777c13188ab11a770ed19da9316f5de2a193b35
htkz <htkzmo@gmail.com>
2020-04-17 23:35:47 -0400

    Connect sign in with backend

--- front-end/js/sign-in-up.js
+++ front-end/js/sign-in-up.js
@@ -9,3 +9,22 @@ $('#signinAnchor').click((event) => {
     $('.form-signup').hide();
     $('.form-signin').fadeIn(1000);
 });
+
+$('#form-signin').submit(async (event) => {
+    event.preventDefault();
+    const username = $('#signinEmail').val();
+    const password = $('#signinPassword').val();
+    try {
+        await $.ajax({
+            url: 'http://localhost:3000/users/account/login',
+            type: 'POST',
+            data: {
+                email: username,
+                hashedPassword: password,
+            },
+        });
+        alert('Correct Username and Password!');
+    } catch (error) {
+        alert(error['responseJSON']['message']);
+    }
+});
