e49999183d84338b4ae0e7d3d78ce26ac00c7385
htkz <htkzmo@gmail.com>
2020-04-18 20:55:48 -0400

    Add cookie parser

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const configRoutes = require('./back-end/routes');
+const cookieParser = require('cookie-parser');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 configRoutes(app);
 
 app.listen(3000, () => {
