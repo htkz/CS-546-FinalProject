08fbbfce1d9349dc2ebd14d6c73823d46faee193
Yuqi Wang <wangyq961129@outlook.com>
2020-04-18 20:18:06 -0400

    Signup input checking

--- front-end/js/signup.js
+++ front-end/js/signup.js
@@ -1,13 +1,16 @@
-async function signUpClick(){
-    const signupForm = document.getElementById('form-signup');
-    const signupEmail = signupForm.signupEmail.value;
-    const userName = signupForm.userName.value;
-    const signupPassword = signupForm.signupPassword.value;
-    const reEnterPassword = signupForm.reEnterPassword.value;
+$('#form-signup').submit(async (event) =>{
+    event.preventDefault();
+    const signupEmail = $('#signupEmail').val();
+    const userName = $('#userName').val();
+    const signupPassword = $('#signupPassword').val();
+    const reEnterPassword = $('#reEnterPassword').val();
 
-    let signupResult;
+    if(!signupEmailChecking(signupEmail)) return;
+    if(!userNameChecking(userName)) return;
+    if(!passwordChecking(signupPassword,reEnterPassword)) return;
+
     try{
-        signupResult = await $.ajax({
+        await $.ajax({
             url : 'http://localhost:3000/users/account/register',
             type : 'POST',
             data: {
@@ -16,19 +19,73 @@ async function signUpClick(){
                 hashedPassword: signupPassword
             }
         });
-    }catch(e){
-        alert(e);
+    }catch(error){
+        alert(error['responseJSON']['message']);
     }
-}
+});
 
 function signupEmailChecking(signupEmail){
-
+    if(!signupEmail) {
+        errorDisplay('No Email Input');
+        return false;
+    }
+    return true;
 }
 
 function userNameChecking(userName){
+    if(!userName){
+        errorDisplay('No Username Input');
+        return false;
+    }
 
+    if( userName.length<3   ||
+        userName.length>16  ||
+        !formatChecking(userName)
+    ){
+        errorDisplay('Illegal username');
+        return false;
+    }
+
+    return true;
 }
 
 function passwordChecking(signupPassword,reEnterPassword){
+    if(!signupPassword){
+        errorDisplay('No Sign-up Password Input');
+        return false;
+    }
 
+    if(!reEnterPassword){
+        errorDisplay('No Re-enter Password Input');
+        return false;
+    }
+
+    if( signupPassword.length<8   ||
+        signupPassword.length>16  ||
+        !formatChecking(signupPassword)
+    ){
+        errorDisplay('Illegal sign-up password');
+        return false;
+    }
+
+    if(signupPassword !== reEnterPassword){
+        errorDisplay('Re-enter password not match');
+        return false;
+    }
+
+    return true;
+}
+
+function errorDisplay(error){
+    alert(error);
+}
+
+function formatChecking(s){
+    for(let c of s){
+        if( !(c.charCodeAt(0)-'a'.charCodeAt(0)>=0 && c.charCodeAt(0)-'z'.charCodeAt(0)<=0)  &&
+            !(c.charCodeAt(0)-'A'.charCodeAt(0)>=0 && c.charCodeAt(0)-'Z'.charCodeAt(0)<=0)  &&
+            !(c.charCodeAt(0)-'0'.charCodeAt(0)>=0 && c.charCodeAt(0)-'9'.charCodeAt(0)<=0) 
+        )   return false;
+    }
+    return true;
 }
