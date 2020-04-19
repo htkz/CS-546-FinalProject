e89af4b9c3a62dff2da00a988f40b7e64f4fe062
htkz <htkzmo@gmail.com>
2020-04-19 00:49:48 -0400

    Bug fix

--- front-end/js/signup.js
+++ front-end/js/signup.js
@@ -1,4 +1,4 @@
-$('#form-signup').submit(async (event) =>{
+$('#form-signup').submit(async (event) => {
     event.preventDefault();
     const signupEmail = $('#signupEmail').val();
     const userName = $('#userName').val();
@@ -7,86 +7,100 @@ $('#form-signup').submit(async (event) =>{
 
     const eCheck = signupEmailChecking(signupEmail);
     const nCheck = userNameChecking(userName);
-    const pCheck = passwordChecking(signupPassword,reEnterPassword);
+    const pCheck = passwordChecking(signupPassword, reEnterPassword);
 
-    if( !eCheck || !nCheck || !pCheck ) return;
+    if (!eCheck || !nCheck || !pCheck) return;
 
-    try{
+    try {
         await $.ajax({
-            url : 'http://localhost:3000/users/account/register',
-            type : 'POST',
+            url: 'http://localhost:3000/users/account/register',
+            type: 'POST',
             data: {
                 userName: userName,
                 email: signupEmail,
-                hashedPassword: signupPassword
-            }
+                hashedPassword: signupPassword,
+            },
         });
-    }catch(error){
-        alert(error['responseJSON']['message']);
+    } catch (error) {
+        alert(error['responseJSON']['error']);
     }
 });
 
-function signupEmailChecking(signupEmail){
+function signupEmailChecking(signupEmail) {
     let temp = document.getElementById('emailRule');
 
-    if(temp.className === 'formatRules') temp.className='hidden';
+    if (temp.className === 'formatRules') temp.className = 'hidden';
 
-    if(!signupEmail) {
-        temp.className='formatRules';
+    if (!signupEmail) {
+        temp.className = 'formatRules';
         return false;
     }
     return true;
 }
 
-function userNameChecking(userName){
+function userNameChecking(userName) {
     let temp = document.getElementById('usernameRule');
 
-    if(temp.className === 'formatRules') temp.className='hidden';
+    if (temp.className === 'formatRules') temp.className = 'hidden';
 
-    if( !userName   ||
-        userName.length<3   ||
-        userName.length>16  ||
+    if (
+        !userName ||
+        userName.length < 3 ||
+        userName.length > 16 ||
         !formatChecking(userName)
-    ){
-        temp.className='formatRules';
+    ) {
+        temp.className = 'formatRules';
         return false;
     }
 
     return true;
 }
 
-function passwordChecking(signupPassword,reEnterPassword){
+function passwordChecking(signupPassword, reEnterPassword) {
     let password = document.getElementById('passwordRule');
     let re_password = document.getElementById('re-passwordRule');
 
-    if(password.className === 'formatRules') password.className='hidden';
+    if (password.className === 'formatRules') password.className = 'hidden';
 
-    if(re_password.className === 'formatRules') re_password.className='hidden';
+    if (re_password.className === 'formatRules')
+        re_password.className = 'hidden';
 
-    if( !signupPassword ||
+    if (
+        !signupPassword ||
         !reEnterPassword ||
-        signupPassword.length<8   ||
-        signupPassword.length>16  ||
+        signupPassword.length < 8 ||
+        signupPassword.length > 16 ||
         !formatChecking(signupPassword)
-    ){
-        password.className='formatRules';
+    ) {
+        password.className = 'formatRules';
         return false;
     }
 
-    if(signupPassword !== reEnterPassword){
-        re_password.className='formatRules';
+    if (signupPassword !== reEnterPassword) {
+        re_password.className = 'formatRules';
         return false;
     }
 
     return true;
 }
 
-function formatChecking(s){
-    for(let c of s){
-        if( !(c.charCodeAt(0)-'a'.charCodeAt(0)>=0 && c.charCodeAt(0)-'z'.charCodeAt(0)<=0)  &&
-            !(c.charCodeAt(0)-'A'.charCodeAt(0)>=0 && c.charCodeAt(0)-'Z'.charCodeAt(0)<=0)  &&
-            !(c.charCodeAt(0)-'0'.charCodeAt(0)>=0 && c.charCodeAt(0)-'9'.charCodeAt(0)<=0) 
-        )   return false;
+function formatChecking(s) {
+    for (let c of s) {
+        if (
+            !(
+                c.charCodeAt(0) - 'a'.charCodeAt(0) >= 0 &&
+                c.charCodeAt(0) - 'z'.charCodeAt(0) <= 0
+            ) &&
+            !(
+                c.charCodeAt(0) - 'A'.charCodeAt(0) >= 0 &&
+                c.charCodeAt(0) - 'Z'.charCodeAt(0) <= 0
+            ) &&
+            !(
+                c.charCodeAt(0) - '0'.charCodeAt(0) >= 0 &&
+                c.charCodeAt(0) - '9'.charCodeAt(0) <= 0
+            )
+        )
+            return false;
     }
     return true;
 }
