9725e0c48bc44bf1f68dd83fab0e79bd839a7c66
htkz <htkzmo@gmail.com>
2020-04-22 16:46:41 -0400

    Add animation for user page

--- public/js/user.js
+++ public/js/user.js
@@ -33,8 +33,27 @@ const logout = async (event) => {
     window.location.replace('http://localhost:3000/entry');
 };
 
+const changeFocus = (id) => {
+    $('.navbar li').each((index, li) => {
+        const $li = $(li);
+        if ($li.attr('id') === id) {
+            $li.attr('class', 'active');
+            $($li.attr('data-id')).fadeIn(1000);
+        } else {
+            $li.attr('class', '');
+            $($li.attr('data-id')).hide();
+        }
+    });
+};
+
 const bindEvents = async () => {
     $('#logoutBtn').click(logout);
+    $('.navbar li').each((index, li) => {
+        const $li = $(li);
+        $li.click(() => {
+            changeFocus($li.attr('id'));
+        });
+    });
 };
 
 const init = async () => {
