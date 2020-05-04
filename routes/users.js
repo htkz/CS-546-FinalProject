8f03292090c732a058adb7eba7380d34a259d660
htkz <htkzmo@gmail.com>
2020-05-04 15:46:05 -0400

    For low version of node

--- routes/users.js
+++ routes/users.js
@@ -112,7 +112,7 @@ router.get('/tickets/friends/:id', async (req, res) => {
     for (friend of friends) {
         try {
             await friendData.getFriendById(friend);
-        } catch {
+        } catch (error) {
             res.status(404).json({ error: `Not found friend ${friend}` });
         }
     }
